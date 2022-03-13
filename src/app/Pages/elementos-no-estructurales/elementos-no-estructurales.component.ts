725dcfdd629087cd4fb02ee5646ee710ea282920
svasquezg <svasquezg@summit.com.co>
2022-03-13 07:37:19 -0500

    Paginacion en tabla

--- src/app/Pages/elementos-no-estructurales/elementos-no-estructurales.component.ts
+++ src/app/Pages/elementos-no-estructurales/elementos-no-estructurales.component.ts
@@ -1,3 +1,4 @@
+import { InfoPisoModel } from './../../models/elementosNoEstructurales/infoPisoModel';
 import { CalcSaRequest } from '../../models/espectroInfo/calcSaRequest';
 import { CalcPeriodosRequest } from './../../models/espectroInfo/calcPeriodosRequest';
 import { GrupoDeUsoModel } from './../../models/espectroInfo/grupoDeUsoModel';
@@ -7,12 +8,14 @@ import { DecimalPipe } from '@angular/common';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { EspectroService } from './../../services/espectro.service';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { Departamento } from 'src/app/models/espectroInfo/departamentoModel';
 import { Municipio } from 'src/app/models/espectroInfo/municipioModel';
 import { ValidatorsService } from 'src/app/services/validators.service';
 import { TipoEstructura } from 'src/app/models/espectroInfo/tipoDeEstructuraModel';
+import { debounceTime } from 'rxjs/operators';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-elementos-no-estructurales',
@@ -23,6 +26,7 @@ export class ElementosNoEstructuralesComponent implements OnInit {
 
   public tittle: string = "Diseño de elementos no estructurales";
   public parametrosSismicosForm: FormGroup;
+  public elementosNoEstructuralesForm: FormGroup;
   public departamentos: Departamento[];
   public municipios: Municipio[];
   public tiposSuelo: TipoSueloFullInfo[];
@@ -39,8 +43,10 @@ export class ElementosNoEstructuralesComponent implements OnInit {
     'prsionSismo',
     'relacionHT'
   ];
+  public panelOpenState: boolean = false;
+  public dataSource = new MatTableDataSource<InfoPisoModel>();
+  @ViewChild(MatPaginator, { static: true }) paginator!: MatPaginator;
 
-  public dataSource = new MatTableDataSource<any>();
   constructor(private fb: FormBuilder,
     private espectroInfoService: EspectroService,
     public spinnerServices: NgxSpinnerService,
@@ -54,6 +60,23 @@ export class ElementosNoEstructuralesComponent implements OnInit {
     this.getTiposEstucturas();
     this.getGruposDeUso();
     this.createParamsSismicosForm();
+    this.createElementosNoEstructuralesForm();
+    this.paginator._intl.itemsPerPageLabel = 'Elementos por página';
+    this.paginator._intl.nextPageLabel = 'Siguiente';
+    this.paginator._intl.previousPageLabel = 'Anterior';
+    this.paginator._intl.firstPageLabel = 'Primera página';
+    this.paginator._intl.lastPageLabel = 'Última página';
+    this.paginator._intl.getRangeLabel = (page: number, pageSize: number, length: number) => {
+      if (length == 0 || pageSize == 0) {
+        return `0 de ${length}`;
+      }
+      length = Math.max(length, 0);
+      const startIndex = page * pageSize;
+      const endIndex = startIndex < length ?
+        Math.min(startIndex + pageSize, length) :
+        startIndex + pageSize;
+      return `${startIndex + 1} - ${endIndex} de ${length}`;
+    }
   }
 
   createParamsSismicosForm() {
@@ -81,11 +104,55 @@ export class ElementosNoEstructuralesComponent implements OnInit {
     this.parametrosSismicosForm.get('municipio').valueChanges.subscribe(data => this.onTipoSueloChange(data));
 
     ///Calculo periodo y sa
-    this.parametrosSismicosForm.get('ht').valueChanges.subscribe(data => this.onParametrosChange(data));
+    this.parametrosSismicosForm.get('ht').valueChanges.pipe(
+      debounceTime(100),
+    ).subscribe(data => this.onParametrosChange(data));
     this.parametrosSismicosForm.get('tipoEstructura').valueChanges.subscribe(data => this.onParametrosChange(data));
     this.parametrosSismicosForm.get('tipoSuelo').valueChanges.subscribe(data => this.onParametrosChange(data));
     this.parametrosSismicosForm.get('municipio').valueChanges.subscribe(data => this.onParametrosChange(data));
-    this.parametrosSismicosForm.get('grupoDeUso').valueChanges.subscribe(data => this.onParametrosChange(data));
+    this.parametrosSismicosForm.get('grupoDeUso').valueChanges.subscribe(data => this.onTaChange());
+    this.parametrosSismicosForm.get('ta').valueChanges.subscribe(data => this.onTaChange());
+  }
+
+  createElementosNoEstructuralesForm() {
+    this.elementosNoEstructuralesForm = this.fb.group({
+      numeroPisos: ['', [Validators.required]],
+      ap: ['', [Validators.required]],
+      rp: ['', [Validators.required]],
+      densidadMuro: ['', [Validators.required]],
+      hViga: ['', [Validators.required]],
+      hEquivalente: ['', [Validators.required]],
+    });
+
+    this.elementosNoEstructuralesForm.get('numeroPisos').valueChanges.pipe(debounceTime(200)).subscribe(data => this.onNumeroPisosChange(data));
+  }
+
+  onNumeroPisosChange(data: number): void {
+    this.spinnerServices.show();
+
+    setTimeout(() => {
+
+      let tempData = new Array<InfoPisoModel>(data);
+
+      for (let i = 0; i < data; i++) {
+        tempData[i] = {
+          losa: `Losa ${i + 1}`,
+          alturaPiso: 0,
+          alturaAcumulada: 0,
+          espesorMuro: 0,
+          pesoMuro: 0,
+          ax: 0,
+          fp: 0,
+          presionSismo: 0,
+          relacionHT: 0
+        }
+      }
+
+      this.dataSource = new MatTableDataSource<InfoPisoModel>(tempData);
+      this.dataSource.paginator = this.paginator;
+      this.spinnerServices.hide();
+    }, 300);
+
   }
 
   onMunicipioChange(municipio: Municipio): void {
@@ -117,7 +184,6 @@ export class ElementosNoEstructuralesComponent implements OnInit {
   private onParametrosChange(data: any) {
 
     let tipoEstructura: TipoEstructura = this.parametrosSismicosForm.get('tipoEstructura').value;
-    let grupoUso: GrupoDeUsoModel = this.parametrosSismicosForm.get('grupoDeUso').value;
 
     let request: CalcPeriodosRequest = {
       ht: this.parametrosSismicosForm.get('ht').value,
@@ -129,6 +195,16 @@ export class ElementosNoEstructuralesComponent implements OnInit {
       alpha: tipoEstructura.coeficiente_alpha
     }
 
+
+    let flag = this.validatorsService.validateRequest(request);
+
+    if (flag) {
+      this.calcPeriodos(request);
+    }
+  }
+
+  private onTaChange() {
+    let grupoUso: GrupoDeUsoModel = this.parametrosSismicosForm.get('grupoDeUso').value;
     let saRequest: CalcSaRequest = {
       aa: this.parametrosSismicosForm.get('coefAa').value,
       av: this.parametrosSismicosForm.get('coefAv').value,
@@ -141,13 +217,7 @@ export class ElementosNoEstructuralesComponent implements OnInit {
       tl: this.parametrosSismicosForm.get('tl').value
     }
 
-    let flag = this.validatorsService.validateRequest(request);
     let flag2 = this.validatorsService.validateRequest(saRequest);
-
-    if (flag) {
-      this.calcPeriodos(request);
-    }
-
     if (flag2) {
       this.calcSa(saRequest);
     }
@@ -268,5 +338,28 @@ export class ElementosNoEstructuralesComponent implements OnInit {
       console.log(error);
       this.spinnerServices.hide();
     });
+  }
+
+  public onAlturaPisoChange(infoPiso: InfoPisoModel, index: number) {
+
+    if (index === 0) {
+      for (let i = 1; i < this.dataSource.data.length; i++) {
+        this.dataSource.data[i].alturaPiso = infoPiso.alturaPiso;
+      }
+    }
+
+    let hAcum = 0;
+    for (let i = 0; i < this.dataSource.data.length; i++) {
+      hAcum += this.dataSource.data[i].alturaPiso;
+      this.dataSource.data[i].alturaAcumulada = hAcum;
+    }
+  }
+
+  public onEspesorMuroChange(infoPiso: InfoPisoModel, index: number) {
+    if (index === 0) {
+      for (let i = 1; i < this.dataSource.data.length; i++) {
+        this.dataSource.data[i].espesorMuro = infoPiso.espesorMuro;
+      }
+    }
   }
 }
---
+++ src/app/models/elementosNoEstructurales/infoPisoModel.ts
@@ -0,0 +1,11 @@
+export interface InfoPisoModel {
+    losa: string;
+    alturaPiso: number;
+    alturaAcumulada: number;
+    espesorMuro: number;
+    pesoMuro: number;
+    ax: number;
+    fp: number;
+    presionSismo: number;
+    relacionHT: number;
+}
